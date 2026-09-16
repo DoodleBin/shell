3791c84797f6503d560fb0519e08292a3ed28a2f
DoodleBin <alexahatcher4@gmail.com>
2026-09-15 19:15:47 -0500

    Added starter code

---
+++ mysh.c
@@ -0,0 +1,46 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// This is the maximum number of arguments your shell should handle for one command
+#define MAX_ARGS 128
+
+int main(int argc, char** argv) {
+  char* line = NULL;     // Pointer that will hold the line we read in
+  size_t line_size = 0;  // The number of bytes available in line
+
+  // Loop forever
+  while (true) {
+    // Print the shell prompt
+    printf("$ ");
+
+    // Get a line of stdin, storing the string pointer in line
+    if (getline(&line, &line_size, stdin) == -1) {
+      if (errno == EINVAL) {
+        perror("Unable to read command line");
+        exit(2);
+      } else {
+        // Must have been end of file (ctrl+D)
+        printf("\nShutting down...\n");
+
+        // Exit the infinite loop
+        break;
+      }
+    }
+
+    // TODO: Execute the command instead of printing it below
+    printf("Received command: %s\n", line);
+  }
+
+  // If we read in at least one line, free this space
+  if (line != NULL) {
+    free(line);
+  }
+
+  return 0;
+}
