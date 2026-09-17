206fbe9291cc7ef55303110a238a48364aaac57b
DoodleBin <alexahatcher4@gmail.com>
2026-09-16 19:46:38 -0500

    Added inside loop to skip spaces

--- mysh.c
+++ mysh.c
@@ -35,41 +35,18 @@ int main(int argc, char** argv) {
       }
     }
 
-  int counter = 0;
-  char** args = malloc(MAX_ARGS*sizeof(char*));
-  //inside loop
-  args[counter] = cur_arg;
-  //once loop ends
-  args[counter+1] = NULL;
-
-  //forking and executing the command
-
-  int curr_process = 0;
-  while (curr_process < counter) {
-    pid_t child_id = fork();
-    if (child_id == 0) {
-      execvp(args[curr_process], args);
-      perror("exec failed");
-      exit(EXIT_FAILURE);
-    } else if (child_id > 0) {
-      int status;
-    wait(&status);
-      printf("%s exited with status %d\n", args[curr_process], WEXITSTATUS(status));
-    } else {
-      perror("fork failed");
-      exit(EXIT_FAILURE);
-    }
-  curr_process++;
-}
-
     // TODO: Execute the command instead of printing it below
     int counter = 0;
-    char** args = malloc(MAX_ARGS * sizeof(char*));
+    char* token = line;
+    char** args = malloc((MAX_ARGS + 1)* sizeof(char*));
     char* cur_arg;
-    for (int i = 1; (cur_arg = strsep(&line, " ")) != NULL; i++) {
-      args[i] = cur_arg;
+    for (int i = 0; (cur_arg = strsep(&token, " ")) != NULL; i++) {
+      if (*cur_arg != '\0') {
+        counter++;
+        args[i] = cur_arg;
+      }
     }
-    args[counter + 1] = NULL;
+    args[counter] = NULL;
     // forking and executing the command
     printf("Received command: %s\n", line);
 
